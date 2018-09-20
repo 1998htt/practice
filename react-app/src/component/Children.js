f6e31c9889f1efd3f39d2a7d3b55258b8bdb110a
1998htt <3133437521@qq.com>
2018-09-20 21:42:12 +0800

    project

--- react-app/src/component/Children.js
+++ react-app/src/component/Children.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-class Children extends Component{
-    render(){
-        return (
-            <div>
-                Children页
-            </div>
-        )
-    }
+// class PageA extends Component {
+//     constructor(props) {
+//         super(props);
+//         console.log(props.match.params.id)
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 A页面
+
+//                 {this.props.match.params.id}
+//             </div>
+//         );
+//     }
+// }
+
+
+const Children = ({match}) =>{
+    return (
+        <div>
+            {
+                match?match.params.num:0
+            }
+            children页面
+        </div>
+    )
 }
+
 export default Children;
--- react-app/src/component/Main.js
+++ react-app/src/component/Main.js
@@ -1,38 +1,94 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link,Switch } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link ,
+    Switch,
+    Redirect,
+    Prompt,
+    NavLink
+} from 'react-router-dom';
 import PageA from './PageA';
 import PageB from './PageB';
 import Home from './Home';
 import Children from './Children';
-class Main extends Component{
-    render(){
+
+const Jump = (props) => {
+    return <Link to={props.to}>jump</Link>
+}
+// const X = (props)=>{
+//     return (
+//         <div>
+//             <button onClick={()=>{
+//                 props.history.push('/')
+//             }}>click</button>
+//         </div>
+//     )
+// }
+
+const X = (props) => {
+    return (
+        <div>
+            <button
+                onClick={()=>{
+                    // console.log(props);
+
+                    props.history.push('/');
+                }}
+            >click</button>
+        </div>
+    )
+}
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            show:false
+        }
+        console.log("+++++",props);
+    }
+
+    render() {
         return (
             <div>
-              main主页面
+                main主页面
                 <Router>
                     <div>
-                        <Link to="/">首页</Link>
+                        <NavLink activeClassName="selected" to="/" isActive={()=>{
+                            console.log(this.props);
+                        }}>首页</NavLink>//改名
+                        ---
+                        <NavLink to="/a/5">跳转A</NavLink>//激活了一个class类名
                         ----
-                        <Link to="/a/2">跳转A</Link>
+                        <NavLink to="/b">跳转B</NavLink>
                         ----
-                        <Link to="/b">跳转B</Link>
-                        ----
-                        <Link to="/children">Children</Link>
-                        {/*<Switch>*/}
-                            {/*<Route path="/a" component={PageA}></Route>*/}
-                            {/*<Route path="/b" component={PageB}></Route>*/}
-                            {/*<Route path="/" component={Home}></Route>*/}
-                        {/*</Switch>*/}
+                        <NavLink to="/render">render</NavLink>
+                        ---
+                        <NavLink to="/children/6">children</NavLink>
+                        ---
+                        <NavLink to="/x">编程式导航</NavLink>
+                        {/* <Switch>
+                            <Route path="/a" component={PageA}></Route>
+                            <Route path="/b" component={PageB}></Route>
+                            <Route path="/" component={Home}></Route>
+                        </Switch> */}
                         <Route exact path="/" component={Home}></Route>
-                        <Route  path="/a/:id" component={PageA}></Route>
+                        <Route path="/a/:id" component={PageA}></Route>
                         <Route path="/b" component={PageB}></Route>
-                        <Route path="/children" children={Children}></Route>
+                        <Route path="/render" render={()=>{
+                            // return <h1>render页</h1>
+                            return <Redirect to="/b" />
+                        }}></Route>
+                        {/* <Route path="/children/:num" children={Children}></Route> */}
+                        <Jump to="/"></Jump>
 
-
+                        <Route path="/x" component={X}></Route>
+                        <Prompt when={this.state.show} message="是否留在当前页面" ></Prompt>//阻止跳转
                     </div>
                 </Router>
             </div>
-        )
+        );
     }
 }
 export default Main;
