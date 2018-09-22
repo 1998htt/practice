45481ee86e982e4dccd3882283f24a08b8a26f65
1998htt <3133437521@qq.com>
2018-09-22 16:20:27 +0800

    meituan

---
+++ meituan/src/component/Home.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { NavBar, Icon, SearchBar,Grid} from 'antd-mobile';
+import './../asset/css/home.css';
+export default class Home extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data:[
+                {
+                    icon:require('./../asset/img/img1.png'),
+                    text:"美食"
+                },
+                {
+                    icon:require('./../asset/img/img2.png'),
+                    text:"电影"
+                },
+                {
+                    icon:require('./../asset/img/img3.png'),
+                    text:"酒店"
+                },
+                {
+                    icon:require('./../asset/img/img4.png'),
+                    text:"娱乐"
+                },
+                {
+                    icon:require('./../asset/img/img5.png'),
+                    text:"外卖"
+                },
+                {
+                    icon:require('./../asset/img/img6.png'),
+                    text:"ktv"
+                },
+                {
+                    icon:require('./../asset/img/img7.png'),
+                    text:"周边游"
+                },
+                {
+                    icon:require('./../asset/img/img8.png'),
+                    text:"丽人"
+                },
+                {
+                    icon:require('./../asset/img/img9.png'),
+                    text:"小吃"
+                },
+                {
+                    icon:require('./../asset/img/img10.png'),
+                    text:"机票"
+                },
+                {
+                    icon:require('./../asset/img/img1.png'),
+                    text:"美食"
+                },
+                {
+                    icon:require('./../asset/img/img2.png'),
+                    text:"电影"
+                },
+                {
+                    icon:require('./../asset/img/img3.png'),
+                    text:"美食"
+                },
+                {
+                    icon:require('./../asset/img/img4.png'),
+                    text:"电影"
+                }
+            ]
+        }
+    }
+    render(){
+        return(
+            <div>
+
+                <NavBar
+                    mode="dark"
+                    leftContent="Back"
+                    rightContent={[
+                        <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+                        <Icon key="1" type="ellipsis" />,
+                    ]}
+                >
+                <SearchBar placeholder="Search" />
+                </NavBar>
+                <Grid data={this.state.data} isCarousel columnNum={5} />
+            </div>
+        )
+    }
+}
