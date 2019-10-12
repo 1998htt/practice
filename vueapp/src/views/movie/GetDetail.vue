6d7994dd1e24f9b113d7efc2372faf48c000f40a
houtingting5 <Httwoshi1998!>
2019-10-12 18:28:06 +0800

    async

--- vueapp/src/views/movie/GetDetail.vue
+++ vueapp/src/views/movie/GetDetail.vue
@@ -29,3 +29,314 @@
 <style>
 
 </style>
+
+
+
+<template>
+    <header class="bg-header">
+        <div class="container">
+            <div class="logo">
+                <img src="../../assets/starlinklogobig.png" alt class="second-img" />
+            </div>
+            <div class="hd-links normal">
+                <div @click.stop="handleClick('home')">
+                    <a>首页</a>
+                    <div class="underline"
+                         v-bind:class="{active : current == 'home'}"></div>
+                </div>
+                <div class="dropdown"
+                     v-on:mouseover="showDropDown"
+                     v-on:mouseleave="hideDropDown"
+                     @click.stop="handleClick('productApps')">
+                    <a>产品服务</a>
+                    <div class="underline"
+                         v-bind:class="{active : current == 'productApps'}"></div>
+                    <transition name="fast">
+                        <div class="dropdown-content"
+                             v-show="show">
+                            <div class="container">
+                                <div class="hdcell-a">
+                                    <h2 class="normal">智能应用</h2>
+                                    <ul class="sm-normal">
+                                        <li v-for="(v,idx) in header.apps"
+                                            :key="idx"
+                                            @click.stop="routerby('productApps','productApps',idx)">
+                                            <a>{{v}}</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="hdcell-b">
+                                    <h2 class="normal">智能硬件</h2>
+                                    <ul class="sm-normal">
+                                        <li v-for="(v,idx) in header.hardwares"
+                                            :key="idx"
+                                            @click.stop="routerby('productApps','productHardware', idx)">
+                                            <a>{{v}}</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="hdcell-c">
+                                    <h2 class="normal">算法类</h2>
+                                    <ul class="sm-normal">
+                                        <li v-for="(v,idx) in header.algorithm"
+                                            :key="idx"
+                                            @click.stop=" routerby('productApps','productAlgo', idx, idx); ">
+                                            <a>{{v}}</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </transition>
+                </div>
+                <div @click.stop="handleClick('document')">
+                    <a>文档中心</a>
+                    <div class="underline"
+                         v-bind:class="{active : current == 'document'}"></div>
+                </div>
+                <div @click.stop="handleClick('joinus')">
+                    <a>加入我们</a>
+                    <div class="underline"
+                         v-bind:class="{active : current == 'joinus'}"></div>
+                </div>
+                <div>
+                    <el-dropdown>
+                        <el-button type="primary"
+                                   class="btn"
+                                   v-show="nickName && (managerUrl.mangerUrl || managerUrl.iotUrl)">
+                            <img class="controlIcon"
+                                 src="../../assets/icons/control.svg" />控制台
+                        </el-button>
+                        <el-dropdown-menu slot="dropdown" class="power">
+                            <el-dropdown-item v-for="(item,index) in list" :key="index" class="item" >
+                                <a :href="item.address" class="address">{{item.name}}</a>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                    <span style="margin-left:23px">|</span>
+                </div>
+                <!--        <div>-->
+                <!--          <el-dropdown trigger="click">-->
+                <!--            <el-button type="primary"-->
+                <!--                       class="btn"-->
+                <!--                      >-->
+                <!--              <img class="controlIcon"-->
+                <!--                   src="../../assets/icons/control.svg" />控制台-->
+                <!--            </el-button>-->
+                <!--            <el-dropdown-menu slot="dropdown" class="power" v-if="list">-->
+                <!--              <el-dropdown-item v-for="(item,index) in list" :key="index" class="item" >-->
+                <!--                <a :href="item.address" class="address">{{item.name}}</a>-->
+                <!--              </el-dropdown-item>-->
+                <!--            </el-dropdown-menu>-->
+                <!--          </el-dropdown>-->
+
+                <!--          <span style="margin-left:23px">|</span>-->
+                <!--        </div>-->
+                <div v-if="!nickName">
+                    <div class="login">
+                        <a @click="authenticate">登录</a>
+                    </div>
+                    <span>|</span>
+                    <div class="login">
+                        <a @click="authenticate">注册</a>
+                    </div>
+                </div>
+                <div v-else>
+                    <el-dropdown @command="handleCommand">
+          <span class="el-dropdown-link">
+            {{nickName}}
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+                        <el-dropdown-menu slot="dropdown" class="logout" id="xx">
+                            <el-dropdown-item>
+                                <el-button type="primary">退出</el-button>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    import { mapGetters, mapActions } from "vuex";
+    import productData from "../../utils/productData";
+    // // import smoothscroll from "smoothscroll-polyfill";
+    import login from "../../utils/login";
+    import logout from "../../utils/logout";
+
+    export default {
+        name: "Header",
+        data: function () {
+            return {
+                aa:false,
+                show: false,
+                logName: "登录",
+                header: productData.header,
+                current: "home",
+                controlUrl: "",
+                // showControl: false,
+                list:[],
+                managerUrl: ""
+            };
+        },
+        created () {
+            this.getCurrentUser();
+            this.current = this.getCurrentRoute();
+            // this.showControlBtn();
+            this.power();
+        },
+        computed: {
+            ...mapGetters( [ "nickName", "navPos", "iotUrl", "mangerUrl" ] )
+        },
+        methods: {
+            ...mapActions( [ "getCurrentUser" ] ),
+            authenticate: function () {
+
+                login( this.$route.fullPath );
+            },
+            handleCommand: async function () {
+                logout( this.$route.fullPath )
+            },
+            getCurrentRoute: function () {
+                if (
+                    this.$route.name == "productAlgo" ||
+                    this.$route.name == "productapps" ||
+                    this.$route.name == "productHardware"
+                ) {
+                    // this.current = "productApps";
+                    return "productApps";
+                } else {
+                    return this.$route.name;
+                }
+            },
+            showDropDown: function () {
+                this.show = true;
+            },
+            hideDropDown: function () {
+                this.show = false;
+            },
+            handleClick: function ( pathname ) {
+                if ( pathname !== "productApps" ) {
+                    this.topFunction();
+                    if ( pathname == "joinus" && this.nickName == "" ) {
+                        let routeData = this.$router.resolve( {
+                            name: pathname,
+                            query: { id: 0 }
+                        } );
+                        window.open( routeData.href, "_blank" );
+                    } else {
+                        this.$router.push( { name: pathname, query: { id: 0 } } );
+                    }
+                } else {
+                    this.show = !this.show;
+                }
+            },
+            topFunction: function () {
+                window.scrollTo( {
+                    top: 0,
+                    // behavior: "smooth"
+                } );
+            },
+            routerby: function ( activename, pathname, value ) {
+                console.log( activename, pathname, value );
+                if ( pathname !== "productAlgo" ) this.topFunction();
+                this.show = !this.show;
+                this.$router.push( {
+                    name: pathname,
+                    query: { id: value }
+                } );
+            },
+            control: function () {
+                let controlUrl;
+                if ( this.mangerUrl.mangerUrl !== '' ) {
+                    console.log(this.mangerUrl,'yes');
+                    controlUrl = this.mangerUrl.mangerUrl;
+                    this.showControl = true;
+                } else if ( this.mangerUrl.iotUrl !== '' ) {
+                    controlUrl = this.mangerUrl.iotUrl;
+                    this.showControl = true;
+                }
+                window.open = controlUrl;
+            },
+            showControlBtn: function () {
+                if ( this.mangerUrl.mangerUrl === "" && this.mangerUrl.iotUrl === "" ) {
+                    this.showControl = false;
+                    console.log( this.showControl );
+                }
+            },
+            power: function () {
+                this.list = [];
+                this.managerUrl = (this.mangerUrl && this.mangerUrl != "")?this.mangerUrl:JSON.parse(localStorage.getItem('managerUrl'));
+                console.log(this.managerUrl, this.managerUrl.mangerUrl,this.managerUrl.iotUrl,'dgye');
+                if(this. managerUrl.mangerUrl &&this. managerUrl.mangerUrl !== '' ) {
+                    let obj = {};
+                    obj.name = '京东星链后台';
+                    obj.address = this.managerUrl.mangerUrl;
+                    this.list.push(obj);
+                }
+                if(this.managerUrl.iotUrl && this.managerUrl.iotUrl !== '' ) {
+                    let obj = {};
+                    obj.name = '京东星链IOT';
+                    obj.address = this.managerUrl.iotUrl;
+                    this.list.push(obj);
+                }
+                console.log(this.list,'地址');
+            }
+        },
+        watch: {
+            $route: function ( to ) {
+                if (
+                    to.name == "productAlgo" ||
+                    to.name == "productapps" ||
+                    to.name == "productHardware"
+                ) {
+                    this.current = "productApps";
+                } else {
+                    this.current = to.name;
+                }
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    @import "~@/styles/main.scss";
+    .btn {
+        padding: 0px;
+        padding-left: 15px;
+        padding-right: 15px;
+        height: 30px;
+        line-height: 30px;
+        margin-top: -3px;
+    }
+    .controlIcon {
+        margin-right: 10px;
+        position: relative;
+        top: 1px;
+    }
+    .login {
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+    .power {
+        margin-top: 1.35rem!important;
+        margin-right:-1rem!important;
+        width: 7.5rem;
+        .address:hover{
+            color: #5B4CF9!important;
+        }
+        .address {
+            color: #222222;
+        }
+    }
+    .el-dropdown {
+        position:relative!important;
+    }
+    .logout {
+        margin-top: 1.35rem!important;
+        width: 7.5rem;
+    }
+
+</style>
--- vuecli3/src/views/Mine.vue
+++ vuecli3/src/views/Mine.vue
@@ -1,15 +1,68 @@
 <template>
-    <div>
-        <h1>hello!!!</h1>
-        <router-link :to="{name:'username',params:{name:'侯亭亭',age:20}}">登录</router-link>
-        <router-link to="/mine/password/侯亭亭/20/3133437521@qq.com">注册</router-link>
-        <router-view></router-view>
-    </div>
+  <div id="app">
+<!--  <el-carousel :interval="6000" arrow="always">-->
+<!--    <el-carousel-item v-for="(img,index) in imgList" :key="index">-->
+<!--    -->
 
+<!--     </el-carousel-item>-->
+<!--    </el-carousel>-->
+      <el-carousel :interval="5000" arrow="always">
+          <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item }}</h3>
+          </el-carousel-item>
+      </el-carousel>
+
+
+
+  </div>
 </template>
+
 <script>
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+export default {
+  name: 'App',
+  data(){
+    return{
+      imgList:[
+        {url:'http://img4.imgtn.bdimg.com/it/u=508387608,2848974022&fm=26&gp=0.jpg'},
+        {url:'http://pic16.nipic.com/20111006/6239936_092702973000_2.jpg'},
+        {url:'http://pic25.nipic.com/20121112/9252150_150552938000_2.jpg'},
+        {url:'http://pic1.win4000.com/wallpaper/c/53cdd1f7c1f21.jpg'}
+      ]
+    }
+  },
+  method:{
 
+  }
+}
 </script>
-<style>
 
+<style>
+    #vv {
+        width: 100px;
+        height: 100px;
+        position:absolute;
+        top:200px;
+        z-index: 33;
+        opacity: 0.9;
+    }
+    img {
+        width:100%;
+    }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
