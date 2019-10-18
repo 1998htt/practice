13d7f3b41594ff701d8d208446c48e5a4173084f
houtingting5 <Httwoshi1998!>
2019-10-18 14:59:15 +0800

    组件独享守卫钩子函数

---
+++ vuecli3/src/views/BeforeRoute.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="test_box">
+    <p @click="go">测试组件内部守卫的作用，点击跳到HelloWorld</p>
+    <h4>{{msg}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg: ""
+        }
+    },
+    // 属于组件自己的生命周期
+    beforeRouteEnter(to,from,next){
+        console.log(to);
+        console.log(from);
+        console.log(next)
+        next(vm =>{
+            vm.msg = 'htt'
+            localStorage.msg = vm.msg;
+            console.log(vm);
+        })
+        
+    },
+    beforeRouteUpdate(to,from,next){
+        console.log(to);
+        console.log(from);
+        console.log(next)
+
+        next(vm =>{
+            vm.msg = '111'
+            localStorage.msg = vm.msg;
+            console.log(vm);
+        })
+        
+    },
+    beforeRouteLeave(to,from,next){
+        console.log(to);
+        console.log(from);
+        console.log(next)
+
+        next(vm =>{
+            vm.msg = '111'
+            localStorage.msg = vm.msg;
+            console.log(vm);
+        })
+        
+    },
+    methods: {
+        go() {
+        console.log(1111)
+        //   编程式导航
+        this.$router.push({ name: 'componentA' })
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
